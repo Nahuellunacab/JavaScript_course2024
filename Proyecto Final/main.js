d6844f47899c224643a8d918db495527670effa0
Nahuel Luna <nahuel.lunacab@gmail.com>
2024-02-27 16:38:45 -0300

    Finalizacion preentrega 2

--- Proyecto Final/main.js
+++ Proyecto Final/main.js
@@ -1,69 +1,104 @@
-const nombre = prompt("Buenas tardes, por favor ingrese su nombre para continuar");
-alert("Buenas tardes " + nombre + ", esta es una plataforma para que usted pueda calcular su registro semanal de recorrido realizado en una actividad fisica.\nRecuerde que los valores que ingrese deben ser expresado en kilometros.\nPara continuar por favor presione 'aceptar'");
+const nombre1 = prompt("Buenas tardes, por favor ingrese su nombre para continuar");
+const apellido1 = prompt("Ingrese su apellido");
+const edad1 = prompt("Ingrese su edad");
+const diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]; 
+let contDias = [0, 0, 0, 0, 0, 0, 0];
 
-let opcion1 = prompt("1. Realizar carga de datos de recorrido.\n2. Guia de usuario.\nPor favor seleccione la opción que deseé para continuar:");
+const persona1 = new Persona(nombre1, edad1, apellido1);
+
+alert("Buenas tardes " + persona1.nombre + ' ' + persona1.apellido +", esta es una plataforma para que usted pueda calcular su registro semanal de recorrido realizado en una actividad fisica.\nRecuerde que los valores que ingrese deben ser expresado en kilometros.\nPara continuar por favor presione 'aceptar'");
+
+let opcion1 = prompt("1. Realizar carga de datos de recorrido.\n2. Día que mas Km se recorrio. \n3. Guia de usuario.\n4.Ver datos de usuario.\n5. SALIR.\nPor favor seleccione la opción que deseé para continuar:");
 let opcion2;
 
-while (opcion1 != 1 && opcion1 != 2) {
+while (opcion1 != 1 && opcion1 != 2 && opcion1 != 3 && opcion1 != 4 && opcion1 != 5) {
     alert("La opción ingresada es incorrecta, por favor vuelva a ingresar una opción");
-    opcion1 = prompt("1. Realizar carga de datos de recorrido.\n2. Guia de usuario.\nPor favor seleccione la opción que deseé para continuar:");
+    opcion1 = prompt("1. Realizar carga de datos de recorrido.\n2. Día que mas Km se recorrio. \n3. Guia de usuario.\n4.Ver datos de usuario.\n5. SALIR.\nPor favor seleccione la opción que deseé para continuar:");
 }
 
 //----------------------------------------------------Primera parte de elección de opcion1----------------------------------------------------------------------------------------------------------------------------------
-if (opcion1 == 1) {
-    opcion2 = prompt("Por favor ingrese el numero dia que quiere realizar la carga de datos recorridos:\n1. Lunes.\n2. Martes.\n3. Miercoles.\n4. Jueves.\n5. Viernes.\n6. Sabado.\n7. Domingo.");
-    while (opcion2 < 1 || opcion2 > 7){
-        alert("La opción ingresada es incorrecta, por favor vuelva a ingresar una opción");
+while (opcion1 != 5){
+    if (opcion1 == 1) {
         opcion2 = prompt("Por favor ingrese el numero dia que quiere realizar la carga de datos recorridos:\n1. Lunes.\n2. Martes.\n3. Miercoles.\n4. Jueves.\n5. Viernes.\n6. Sabado.\n7. Domingo.");
+        while (opcion2 < 1 || opcion2 > 7){
+            alert("La opción ingresada es incorrecta, por favor vuelva a ingresar una opción");
+            opcion2 = prompt("Por favor ingrese el numero dia que quiere realizar la carga de datos recorridos:\n1. Lunes.\n2. Martes.\n3. Miercoles.\n4. Jueves.\n5. Viernes.\n6. Sabado.\n7. Domingo.");
+        }
+        
+    //-----------------------------------------------------Segunda parte de elección de opcion2----------------------------------------------------------------------------------------------------------------------------------
+        let valor;
+        switch(opcion2){
+            case "1":
+                valor = cargarDato(0);
+                break;
+            case "2":
+                valor = cargarDato(1);
+                break;
+            case "3":
+                valor = cargarDato(2);
+                break;
+            case "4":
+                valor = cargarDato(3);
+                break;
+            case "5":
+                valor = cargarDato(4);
+                break;
+            case "6":
+                valor = cargarDato(5);
+                break;
+            case "7":
+                valor = cargarDato(6);
+                break;
+        }
+    }
+
+    else if (opcion1 == 2){
+        diaMasRecorrido();
+    }
+
+    else if (opcion1 == 3){
+        alert("La plataforma funciona de la siguiente manera:\n Usted debe seleccionar el día que desea realizar la carga de datos y luego usted podrá ver estadisticas de su entrenamiento semanal en base a los recorridos realizados.");
     }
     
-//-----------------------------------------------------Segunda parte de elección de opcion2----------------------------------------------------------------------------------------------------------------------------------
-    switch(opcion2){
-        case 1:
-            let valor1 = 0;
-            valor1 = cargarDato("Lunes");
-            alert("La cantidad de kilometros del dia Lunes es de " + valor1);
-            break;
-        case 2:
-            let valor2 = 0;
-            valor2 = cargarDato("Martes");
-            alert("La cantidad de kilometros del dia Martes es de " + valor2);
-            break;
-        case 3:
-            let valor3 = 0;
-            valor3 = cargarDato("Miercoles");
-            alert("La cantidad de kilometros del dia Miercoles es de " + valor3);
-            break;
-        case 4:
-            let valor4 = 0;
-            valor4 = cargarDato("Jueves");
-            alert("La cantidad de kilometros del dia Jueves es de " + valor4);
-            break;
-        case 5:
-            let valor5 = 0;
-            valor5 = cargarDato("Viernes");
-            alert("La cantidad de kilometros del dia Viernes es de " + valor5);
-            break;
-        case 6:
-            let valor6 = 0;
-            valor6 = cargarDato("Sabado");
-            alert("La cantidad de kilometros del dia Sabado es de " + valor6);
-            break;
-        case 7:
-            let valor7 = 0;
-            valor7 = cargarDato("Domingo");
-            alert("La cantidad de kilometros del dia domingo es de " + valor7);
-            break;
+    else if (opcion1 == 4) {
+        alert("Nombre: " + persona1.nombre+ "\nApellido: " + persona1.apellido + "\nEdad: " + persona1.edad);
     }
-}
 
-else if (opcion1 == 2){
-    alert("La plataforma funciona de la siguiente manera:\n Usted debe seleccionar el día que desea realizar la carga de datos y luego usted podrá ver estadisticas de su entrenamiento semanal en base a los recorridos realizados.");
+    opcion1 = prompt("1. Realizar carga de datos de recorrido.\n2. Día que mas Km se recorrio. \n3. Guia de usuario.\n4.Ver datos de usuario.\n5. SALIR.\nPor favor seleccione la opción que deseé para continuar:");
 
 }
+
+alert("Muchas gracias por su visita!");
 
 //----------------------------------------------------------------------Funciones-----------------------------------------------------------------------------------------------------------------------------------------------
 function cargarDato(dia) {
-    let carga = prompt("Por favor ingrese la cantidad de kilometros que desee agregar al dia " + dia);
+    let carga = prompt("Por favor ingrese la cantidad de kilometros que desee agregar al dia " + diasSemana[dia]);
+    contDias[dia] += parseInt(carga);
+    alert("Se sumo una cantidad de " + carga + " Kilometros al dia " + diasSemana[dia] + ".\nEl total acumulado de este día es: " + contDias[dia] + "Km");
     return carga;
+}
+
+// Función constructora de Objeto persona
+function Persona(nombre, edad, apellido) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.apellido = apellido;
+}
+
+// Función para busqueda de dia que mas se corrio.
+function diaMasRecorrido(){
+    let diaMayor = 0;
+    let valorMayor = 0;
+    for (let i = 0; i < contDias.length; i++){
+        if (contDias[i] >= diaMayor){
+            diaMayor = i;
+            valorMayor = contDias[i];
+        }
+    }
+    if (valorMayor > 0){
+        alert("El dia que mas se recorrio kilometros fue el " + diasSemana[diaMayor] + " con una cantidad de " + valorMayor + " kilometros.");
+    }
+    else {
+        alert("Aún no se han registrado carga de datos.");
+    }
 }
